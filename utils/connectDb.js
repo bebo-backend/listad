0f66f764e731c22b3b80a265854a8d3a96799173
gks <gks2018@yahoo.com>
2021-04-20 02:03:39 +0100

    fixiing mongo atlas

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -14,9 +14,13 @@ async function dbConnect() {
 
   /* connecting to our database */
 
+
+//mongodb+srv://shadrach:<password>@cluster0.s0lbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+
   
 //  const db = await mongoose.connect("mongodb://127.0.0.1:27017/", {
- const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16.com@cluster0.s0lbg.mongodb.net/postlist_db?retryWrites=true&w=majority", {
+ const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16.com@cluster0.s0lbg.mongodb.net/listad_db?retryWrites=true&w=majority", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
