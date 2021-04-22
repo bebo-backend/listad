db8beb89685c51a3563b1e014fb286577b98ce6d
gks <gks2018@yahoo.com>
2021-04-22 04:10:22 +0100

    fix text

--- utils/connectDb.js
+++ utils/connectDb.js
@@ -15,12 +15,10 @@ async function dbConnect() {
   /* connecting to our database */
 
 
-//mongodb+srv://shadrach:<password>@cluster0.s0lbg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 
+ //const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16.com@cluster0.mfado.mongodb.net/listad_database?retryWrites=true&w=majority", {
 
-  
-//  const db = await mongoose.connect("mongodb://127.0.0.1:27017/", {
- const db = await mongoose.connect("mongodb+srv://shadrach:Shadrach16.com@cluster0.mfado.mongodb.net/listad_database?retryWrites=true&w=majority", {
+const db = await mongoose.connect("mongodb://127.0.0.1:27017/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
